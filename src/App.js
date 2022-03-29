c2dae082ca545e03b32277c43be3f4f7eb260c85
youngseoKim-kr <ysk5754@naver.com>
2022-03-29 14:32:52 +0900

    Add : 프론트엔드  초기세팅

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import Search from './pages/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/" element={<Main />} />
-      <Route path="/Detail/:id" element={<Detail />} />
-      <Route path="/Search" element={<Search />} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Detail" element={<Detail />} />
+        <Route path="/Search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
