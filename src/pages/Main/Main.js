d472f73be8f7625ab672adedf3c3b8bc13c06c23
youngseoKim-kr <ysk5754@naver.com>
2022-04-01 15:06:19 +0900

    Add :  비슷한 작품 컴포넌트 구현

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,12 @@
+import Gallery from '../Detail/Gallery/Gallery';
+import SimilarMovie from '../Detail/SimilarMovie/SimilarMovie';
+
+function Main() {
+  return (
+    <div>
+      <SimilarMovie />
+      <Gallery />
+    </div>
+  );
+}
+export default Main;
