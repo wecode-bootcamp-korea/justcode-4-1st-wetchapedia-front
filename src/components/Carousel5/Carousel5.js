af7016b3b26bdcf268ab236420d9abb2ff2527f4
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-04-04 04:10:08 +0900

    ADD: fetch를 통해 API와 연결하여 map을 통하여 표시하게 구현

--- src/components/Carousel5/Carousel5.js
+++ src/components/Carousel5/Carousel5.js
@@ -34,6 +34,28 @@ const Carousel5 = props => {
     setRightButtonOn('');
     setLeftButtonOn('');
   }
+
+  const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    fetch(
+      `http://localhost:7000/Main/Carousel?CategoryId=${props.categoryId}&limit=${props.limit}`,
+      {
+        method: 'GET',
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        let dataForCarousel = data.CarouselData;
+        let sequenceNumber = 1;
+        dataForCarousel.map(data => {
+          data.release_year = data.release_date.split('/')[0];
+          data.sequenceNumber = sequenceNumber;
+          sequenceNumber += 1;
+        });
+        setMovieList(dataForCarousel);
+      });
+  }, []);
+
   return (
     <div className={styles.Carousel5Wrapper}>
       <div className={styles.DivForButton}>
@@ -57,109 +79,24 @@ const Carousel5 = props => {
           <img src="/image/arrow.svg" />
         </button>
       </div>
-      <div className={styles.Title}>위챠 Top 10 영화</div>
+      <div className={styles.Title}>{props.categoryName}</div>
       <div
         className={styles.SliderWrapper}
         ref={SliderWrapperRef}
         onScroll={checkScrollPos}
       >
-        {/*레이아웃 완료. 여기 맵만 해주면 끝남.*/}
-        <MovieCard
-          title="서울의 연인들"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="1"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/28.png"
-        />
-        <MovieCard
-          title="탐정교실"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="2"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/22.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="3"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/24.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="4"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/12.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="5"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/34.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="6"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/40.png"
-        />
-        <MovieCard
-          title="서울의 연인들"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="7"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/23.png"
-        />
-        <MovieCard
-          title="탐정교실"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="8"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/50.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="9"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/4.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="10"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/25.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="11"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/22.png"
-        />
-        <MovieCard
-          title="맨 인 더 룸 3"
-          releasedYear="2022"
-          countryName="한국"
-          averageRatingScore="3.5"
-          sequenceNumber="12"
-          imgUrl="https://raw.githubusercontent.com/nsoarim/wetchaimage/main/poster/45.png"
-        />
+        {movieList.map(movie => {
+          return (
+            <MovieCard
+              title="테스트"
+              releasedYear={movie.release_year}
+              countryName={movie.country_name}
+              averageRatingScore={movie.count}
+              sequenceNumber={movie.sequenceNumber}
+              imgUrl={'https://' + movie.poster_url}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Carousel5/MovieCard/MovieCard.js
+++ src/components/Carousel5/MovieCard/MovieCard.js
@@ -2,10 +2,7 @@ import styles from './MovieCard.module.scss';
 
 const MovieCard = props => {
   return (
-    <div
-      //style={{ width: props.MovieCardWrapperWidth }}
-      className={styles.MovieCardWrapper}
-    >
+    <div className={styles.MovieCardWrapper}>
       <div className={styles.PosterBox}>
         <div className={styles.SequenceNumberBox}>{props.sequenceNumber}</div>
         <img className={styles.PosterImage} src={props.imgUrl} />
